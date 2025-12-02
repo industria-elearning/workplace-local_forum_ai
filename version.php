2c0e6e9e037eff2927b5ebcac2ceab062d45c2ed
PieroLS04 <piero@datacurso.com>
2025-12-02 10:23:23 -0500

    chore: bump version to 2025120200, tag release 1.0.1, and restrict support from Moodle 4.5 to 5.1

--- version.php
+++ version.php
@@ -25,10 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_forum_ai';
-$plugin->release = '1.0.0';
-$plugin->version = 2025112300;
+$plugin->release = '1.0.1';
+$plugin->version = 2025120200;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
+$plugin->supported = [405, 501];
 $plugin->dependencies = [
     'aiprovider_datacurso' => 2025100201,
 ];
